cd2f12f4eeeba73e4408703b57773b2ab69a6cf3
YasinVighio <yasin.ahmed01@gmail.com>
2024-02-25 20:06:18 +0500

    Theme update with state value

--- src/LearningBoardComponents/LearningBoard.js
+++ src/LearningBoardComponents/LearningBoard.js
@@ -7,12 +7,20 @@ import msgs from './Messages';
 function LearningBoard(){
     let [counter, setCounter] = useState(0);
 
+    let color;
+
     if(counter>=msgs.length || counter<0){
         setCounter(0);
     }
 
+    if(counter%2 == 0){
+        color='blackBoard';
+    } else {
+        color='otherColor';
+    }
+
     return(
-        <div className="learningBoard">
+        <div className={`learningBoard ${color}`}>
             <LearningBoardMessage index={counter}/>
             <button  className='showMsgButon btLeft' onClick={
                 ()=>setCounter(counter-1)
